14418776dfa2756141d14980a03ec3b055a74ffe
Besenok-13 <artbondar2003@gmail.com>
2022-04-16 17:37:15 +0300

    Cool Commit

--- Tenderhack/Game/game.py
+++ Tenderhack/Game/game.py
@@ -25,3 +25,44 @@ def game(
 
     for i in to_dell:
         gamers.pop(i)
+
+
+class Kot:
+    def __init__(self):
+        self.current_price = 10000
+        self.start_price = 10000
+        self.percent = 2
+        self.last = 0
+        self.steps = 5
+        self.isEnded = False
+
+    def low(self, lst):
+        self.last = lst
+        self.steps -= 1
+        self.current_price -= self.start_price * (self.percent / 100)
+        print("lowed!", self.current_price, self.steps)
+        return self.current_price
+
+    def get_current(self):
+        return self.current_price
+
+    def get_last(self):
+        return self.last
+
+    def update(self):
+        if self.steps < 1:
+            self.stop()
+
+    def stop(self):
+        self.isEnded = True
+        print(self.current_price)
+        print(self.last)
+        print("ENDED!")
+
+
+kt = Kot()
+while not kt.isEnded:
+    kt.update()
+    a = int(input())
+    if a > 0:
+        kt.low(a)
--- Tenderhack/NN/NN.py
+++ Tenderhack/NN/NN.py
@@ -1,3 +1,66 @@
-from sklearn.neural_network import MLPClassifier
+# from sklearn.neural_network import MLPClassifier
+# coding=utf-8
+# TheGodEye = MLPClassifier()
+def OneMix():
+    import random
 
-TheGodEye = MLPClassifier()
+    import matplotlib.pyplot as plt
+    import numpy as np
+    from deap import algorithms, base, creator, tools
+
+    ONE_MAX_LENGHT = 100
+    POPULATION_SIZE = 300
+    MAX_GENERATIONS = 50
+    P_CROSSOVER = 0.9
+    P_MUTATION = 0.1
+
+    creator.create("FitnessMax", base.Fitness, weights=(1.0,))
+    creator.create("Individual", list, fitness=creator.FitnessMax)
+
+    def oneMaxFitnes(individual):
+        return (sum(individual),)  # кортеж
+
+    toolbox = base.Toolbox()
+    toolbox.register("zeroOrOne", random.randint, 0, 1)
+    toolbox.register(
+        "IndividualCreator",
+        tools.initRepeat,
+        creator.Individual,
+        toolbox.zeroOrOne,
+        ONE_MAX_LENGHT,
+    )
+    toolbox.register(
+        "populationCreator", tools.initRepeat, list, toolbox.IndividualCreator
+    )
+
+    population = toolbox.populationCreator(n=POPULATION_SIZE)
+
+    toolbox.register("evaluate", oneMaxFitnes)
+    toolbox.register("select", tools.selTournament, tournsize=5)
+    toolbox.register("mate", tools.cxOnePoint)
+    toolbox.register("mutate", tools.mutFlipBit, indpb=1.0 / ONE_MAX_LENGHT)
+
+    stats = tools.Statistics(lambda x: x.fitness.values)
+    stats.register("max", np.max)
+    stats.register("avg", np.mean)
+
+    population, logbook = algorithms.eaSimple(
+        population,
+        toolbox,
+        cxpb=P_CROSSOVER,
+        mutpb=P_MUTATION,
+        ngen=MAX_GENERATIONS,
+        stats=stats,
+        verbose=True,
+    )
+
+    MaxPopVal, MeanPopVal = logbook.select("max", "avg")
+    print(MaxPopVal)
+    plt.plot(MaxPopVal, color="red")
+    plt.plot(MeanPopVal, color="blue")
+    plt.xlabel("Поколения")
+    plt.ylabel("Макс/ср")
+    plt.show()
+
+
+OneMix()
