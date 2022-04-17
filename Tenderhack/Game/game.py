c53b7da0ba647cb0c5a897e8e0234b5d8a8eb53a
Besenok-13 <artbondar2003@gmail.com>
2022-04-17 11:35:10 +0300

    Update BD

--- Tenderhack/Game/game.py
+++ Tenderhack/Game/game.py
@@ -1,19 +1,27 @@
 class Kot:
-    def __init__(self):
-        self.current_price = 10000
-        self.start_price = 10000
-        self.percent = 2
-        self.last = 0
-        self.moves = 1000
+    def __init__(self, start, perc):
+        self.start_price = start
+        self.current_price = start
+        self.percent = perc
+        self.last = ""
+        self.moves = 0
         self.isEnded = False
         self.step = self.start_price * self.percent / 100
+        self.bots = []
+        self.history = []
+        self.in_book = dict()  # {'stop_percent': bots[], ...}
 
     def low(self, lst):
+        if self.last == lst:
+            return 0
+        self.history.append([self.moves, self.current_price])
         self.last = lst
-        self.moves -= 1
+        self.moves += 1
         self.current_price -= self.start_price * (self.percent / 100)
-        print(self.last, "lowed!", self.current_price, self.moves)
         return self.current_price
+
+    def addBot(self, ID, floor):
+        self.bots.append(Bot(ID, floor, self))
 
     def get_current(self):
         return self.current_price
@@ -27,8 +35,10 @@ class Kot:
     def update(self):
         if self.isEnded:
             return 0
-        if self.current_price < 0:
+        if self.current_price <= 0:
             self.stop()
+        for bot in self.bots:
+            bot.update(self)
 
     def imhere():
         print("imhere")
@@ -43,47 +53,98 @@ class Kot:
 class Bot:
     def __init__(self, ID, fl, kt):
         self.login = ID
+        self.fl = fl
         self.floor = fl * kt.start_price / 100
         self.timer = 300  # FOR DELAY
+        self.awaiting = False
+        if f"{fl}" in kt.in_book.keys():
+            temp = kt.in_book[f"{fl}"] + [self]
+        else:
+            temp = [self]
+        kt.in_book[f"{fl}"] = temp
 
     def PRESS_FUCKING_BUTTON(self, kot):
         kot.low(self.login)
-        kot.update()
 
     def update(self, kot):
-        if kot.isStopped():
-            return 0
-        curr_price = kot.get_current()
-        next_price = curr_price - kot.step
+        next_price = kot.current_price - kot.step
+        if next_price - kot.step < self.floor and not self.awaiting:
+            if len(kot.in_book[f"{self.fl}"]) > 2:
+                if kot.in_book[f"{self.fl}"][0] == self and self.login != kot.last:
+                    self.timer = 300
+                elif kot.in_book[f"{self.fl}"][0] == self:
+                    self.timer = 300
+                else:
+                    self.timer = 1000
+                self.awaiting = True
+
         if next_price < self.floor:
-            pass
+            return 0
         else:
-            self.PRESS_FUCKING_BUTTON(kot)
+            if self.delay():
+                self.PRESS_FUCKING_BUTTON(kot)
+
+    def delay(self):
+        self.timer -= 1
+        if self.timer <= 0:
+            self.timer = 150
+            self.awaiting = False
+            return True
+        return False
 
 
-kt = Kot()
-bot1 = Bot("bot N1", 30, kt)
-bot2 = Bot("bot N2", 30, kt)
-for i in range(30):
-    bot1.update(kt)
-    bot2.update(kt)
-    print(kt.current_price)
-kt.low("me")
-print(kt.current_price)
+# kts = []
+# kts.append(Kot(10000, 2))
+
+# kts[0].addBot("bot N1", 88)
+# kts[0].addBot("bot N2", 88)
+# kts[0].addBot("bot N3", 88)
+# for kt in kts:
+#    kt.update()
 
 
-def generate_pict():
-    import json
+""" 
+TODO  
+ 
+Queue for bots to prevent permanent losses 
+last step must be booked for first bot(but not for humans) 
+also there should be delay for last step 
+ 
+"""
 
-    import numpy as np
-    import plotly
-    import plotly.express as px
 
-    x = np.arange(0, 5, 0.1)
+class Test:
+    def __init__(self, x, y):
+        self.x = []
+        self.y = []
+        self.kts = [Kot()]
+        self.kts[0].addBot("bot N1", 30)
+        self.kts[0].addBot("bot N2", 30)
 
-    def f(x):
-        return x ** 2
+    def test_play(self):
 
-    fig = px.scatter(x=x, y=f(x))
-    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
-    return graphJSON
+        for kt in self.kts:
+            kt.update()
+            # self.y.append(kt.current_price)
+            self.y.append(kt.current_price)
+            if len(self.x):
+                self.x.append(self.x[-1] + 1)
+            else:
+                self.x.append(1)
+
+    def generate_pict(self):
+        import json
+
+        import numpy as np
+        import plotly
+        import plotly.express as px
+
+        xx = np.array(self.x)
+        yy = np.array(self.y)
+
+        # def f(x):
+        #    return x ** 2
+        if len(xx) and len(yy):
+            fig = px.scatter(x=xx, y=yy)
+            graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+            return graphJSON
--- Tenderhack/Website/models.py
+++ Tenderhack/Website/models.py
@@ -3,9 +3,29 @@ from sqlalchemy import func
 
 from . import db
 
+Kots_Users = db.Table(
+    "favorite_user_resipes",
+    db.Column("User_id", db.Integer, db.ForeignKey("user.id")),
+    db.Column("Kot_id", db.Integer, db.ForeignKey("kot.id")),
+)
+
 
 class User(db.Model, UserMixin):  # UserMixin для пихания класса User в flask
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
+    kots = db.relationship("Kot", secondary=Kots_Users, backref="kots")
+    first = db.Column(db.Boolean)
+
+
+class Kot(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(10000))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    #   user.id пишется с маленькой буквы, так как мы используем db.ForeignKey а в нём в названии класса большие буквы превращаются в маленькие
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    participaints = db.relationship(
+        "User", secondary=Kots_Users, backref="participaints"
+    )
+
