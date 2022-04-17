bb3cc1c97b4ae579936b266f7566f490c245679e
Besenok-13 <artbondar2003@gmail.com>
2022-04-17 11:34:52 +0300

    Update BD

--- Tenderhack/Website/views.py
+++ Tenderhack/Website/views.py
@@ -1,18 +1,36 @@
 from flask import Blueprint, flash, redirect, render_template, request, url_for
 from flask_login import current_user, login_required
 
-from ..Game.game import generate_pict
+from ..Game.game import Test
 from . import db
 from .models import *
 
 views = Blueprint("views", __name__)
 
+test = Test([], [])
+
 
 @views.route("/", methods=["GET", "POST"])
 @login_required
 def home():
+    # for i in range(10000):
+    #    return render_template(
+    #        "home.html", user=current_user, graphJSON=test.generate_pict()
+    #    )
 
-    return render_template("home.html", user=current_user, graphJSON=generate_pict())
+    if request.method == "POST":
+        for i in range(10000):
+            test.test_play()
+            redirect(url_for("views.home"))
+
+    return render_template(
+        "home.html", user=current_user, graphJSON=test.generate_pict()
+    )
+
+
+@views.route("/create_game", methods=["GET", "POST"])
+def create_game():
+    
 
 
 @views.route("/game/<int:game_id>/<int:user_id>", methods=["GET", "POST"])
